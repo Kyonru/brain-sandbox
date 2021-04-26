12a69c41e7668e6e1fbbd7fcfedfc985d8e579b7
Roberto Amarante <roberto.amarante@flatironsdevelopment.com>
2021-04-25 22:25:11 -0400

    ADD: jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // moduleDirectories: ['node_modules', 'src'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/src/**/*.spec.ts'],
 };
